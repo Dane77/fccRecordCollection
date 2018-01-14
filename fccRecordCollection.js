0aad0b0f101cb0dad202cd205ccd5a145d99edf6
Dane77 <bolesd77@gmail.com>
2018-01-13 23:28:12 -0500

    Create fccRecordCollection.js

---
+++ fccRecordCollection.js
@@ -0,0 +1,13 @@
+function updateRecords(id, prop, value) {
+  if (prop === "tracks" && value !== "") {
+   if(collection[id][prop]) {
+    collection[id][prop].push(value);
+   }
+   else {
+    collection[id][prop]=[value];
+   }
+  } else if (value !== "") {
+    collection[id][prop] = value;
+  } else {
+    delete collection[id][prop];
+  }
